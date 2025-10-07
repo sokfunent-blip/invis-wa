266b701f6d47314f14f6e617d1457d5e57ae051b
sokfunent-blip <sokfun.ent@gmail.com>
2025-10-07 23:40:43 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,4 +1,4 @@
 global.prefa = ['','!','.',',','🐤','🗿']
-global.owner = ['6283143851170']
+global.owner = ['6281946556343']
 global.gambar = ""
 // GANTI NO OWNER DENGAN NOMOR MU LALU RUNN SEPERTI BIASA !!!
